61b3f867c76f42c53843e0024b8e5b3c1b03e50b
Fernando <84139372+Fernando1403@users.noreply.github.com>
2023-10-02 20:54:05 -0300

    Update planos-opcao-2.jsx

--- src/interpolacao/planos/planos-opcao-2.jsx
+++ src/interpolacao/planos/planos-opcao-2.jsx
@@ -10,10 +10,10 @@ function PlanosOpcao(){
               <h3 className="font-1-xl cor-12">BÁSICO</h3>
               <span className="font-1-xl cor-5">R$ XXX <span className="font-1-xs cor-3">mensal</span></span>
               <ul className="font-2-m cor-11">
-                <li>Lorem ipsum dolor mit</li>
-                <li>Lorem ipsum dolor mit</li>
-                <li>Lorem ipsum dolor mit</li>
-                <li>Lorem ipsum dolor mit</li>
+                <li>Gerenciamento de Culturas</li>
+                <li>Sugestões para reduções de custo</li>
+                <li>Blog</li>
+                <li>Previsão de colheita</li>
               </ul>
               <a href="/planos" className="botao-planos botao-fundo-branco">Escolher plano</a>
           </div>
@@ -21,12 +21,12 @@ function PlanosOpcao(){
               <h3 className="font-1-xl cor-p3">AVANÇADO</h3>
               <span className="font-1-xl cor-0">R$ XXX <span className="font-1-xs cor-4">mensal</span></span>
               <ul className="font-2-m cor-0">
-                <li>Lorem ipsum dolor mit</li>
-                <li>Lorem ipsum dolor mit</li>
-                <li>Lorem ipsum dolor mit</li>
-                <li>Lorem ipsum dolor mit</li>
-                <li>Lorem ipsum dolor mit</li>
-                <li>Lorem ipsum dolor mit</li>
+                <li>Gerenciamento de Culturas</li>
+                <li>Sugestões para reduções de custo</li>
+                <li>Blog</li>
+                <li>Previsão de colheita</li>
+                <li>Gráficos dinâmicos em Power BI</li>
+                <li>Consultoria via Whatsapp</li>
               </ul>
               <a href="/planos" className="botao-planos botao-fundo-verde">Escolher plano</a>
           </div>
